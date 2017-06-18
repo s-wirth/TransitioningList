ede05155c6263af5aacab7f9a74e4490a889dfc1
Anna-Sophia Wirth <anna-sophiawirth@AnnaSophiasMBP.fritz.box>
2017-06-18 18:14:09 +0200

    Takes out display from jquery, adds to css

--- js/main.js
+++ js/main.js
@@ -1,8 +1,6 @@
 $(document).ready(function() {
     var containerObj = $(".container");
-    var formObj = $(".edition-choices");
     var choiceDiv = $(".edition-chosen");
-    var textObj = $(".thank-you-text");
     $(".buy-button").click(function(){
         var edition = $( "input[type=radio]:checked" ).val();
         containerObj
@@ -10,9 +8,6 @@ $(document).ready(function() {
             .addClass("container--thank-you")
             .css("height", "300")
             .animate({height: "130px"});
-        formObj.hide();
-        textObj.css("display", "block");
-        choiceDiv.css("display", "block");
         (choiceDiv).text(edition + " Edition");
     });
 });
